9679fb8e393ce97dc06bb8b635fd7faf12318e2e
Bogdoing <Bogdoingg@gmal.com>
2024-01-12 22:02:26 +0300

    add cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express")
+const cors = require('cors')
 const app = express()
 
 require('dotenv').config()
 
 app.use(express.json())
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.json({
